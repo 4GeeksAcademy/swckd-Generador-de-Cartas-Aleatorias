9a14f395c52d6df41eda81146bb264e68c6f85ac
Alejandro <aleckbeu@gmail.com>
2024-03-26 07:39:58 +0000

    Alejandro

--- src/app.js
+++ src/app.js
@@ -7,5 +7,29 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let randomPalo = numeroAleatorio(PALOS.length);
+  console.log(randomPalo);
+  let color;
+  if (randomPalo <= 1) {
+    color = "red";
+  } else {
+    color = "black";
+  }
+
+  divsPalos[0].innerHTML = PALOS[randomPalo];
+
+  divsPalos[0].classList.add(color);
+  divsPalos[1].classList.add(color);
+
+  divsPalos[1].innerHTML = divsPalos[0].innerHTML;
+  divNumero[0].innerHTML = NUMEROS[numeroAleatorio(NUMEROS.length)];
 };
+const PALOS = ["♥", "♦", "♣", "♠"];
+const NUMEROS = [1, 2, 3, 4, 5, 6, 7, 8, 9, "J", "K", "Q"];
+
+let divsPalos = document.getElementsByClassName("palos");
+let divNumero = document.getElementsByClassName("numero");
+
+function numeroAleatorio(length) {
+  return Math.floor(Math.random() * length);
+}
